e8ad2ee73cf40f036cd35a936ebf75b3bd0220a0
GRMCosta <rafa_gian2011@hotmail.com>
2019-10-03 14:31:27 -0300

    ProjectV9

---
+++ ProjectSpring/src/main/java/com/projectspring/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.projectspring.course.repositories;
+
+import com.projectspring.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+
+
+}
